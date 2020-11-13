d5d9e502973b4a77767f004006a053718df00dbe
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-11-13 12:59:28 +0100

    Programma finito ma non funzionante

--- Gianluca_GitHub/Program.cs
+++ Gianluca_GitHub/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace Gianluca_GitHub
 {
@@ -6,8 +9,79 @@ namespace Gianluca_GitHub
     {
         static void Main(string[] args)
         {
-           // Lister : in ascolto quando si parla dei server
-           // EndPoint 
-        }
-    }
-}
+            // Lister : in ascolto quando si parla dei server
+            // EndPoint : identifica una coppia IP/Porta
+
+            // Creare il mio socketlistene
+            // 1) specifico che versione ip
+            // 2) il tipo di socket. Stream.
+            // 3) protocollo a livello di trasporto
+            Socket listenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            // confing: IP deve ascoltare. Possiamo usare l'opzione Any: ascolta da tutte le interfaccie all'interno del mio pc
+            IPAddress ipaddr = IPAddress.Any;
+
+            // config: devo configurare l'EndPoint
+            IPEndPoint ipep = new IPEndPoint(ipaddr, 23000);
+
+
+            // config: Bind -> collegamento
+            // listenerSocket lo collego all'endpoint che ho appena configurato
+            listenerSocket.Bind(ipep);
+
+            // Mettere in ascolto il server
+            // parametro: il numero massimo di connessioni da mettere in coda.
+            listenerSocket.Listen(5);
+            Console.WriteLine("server in ascolto");
+            Console.WriteLine("In attesa di connessione da parte del client");
+
+            //istruzioni bloccante
+            //restituisce una variabile di tipo socket.
+            Socket client = listenerSocket.Accept();
+
+            Console.WriteLine("Client IP: " + client.RemoteEndPoint.ToString());
+
+            // mi attrezzo per riceve un messaggio dal client
+            // siccome è di tipo stream io riceverò dei byte, o meglio un byte array
+            // riceverò anche il numero di byte.
+            byte[] buff = new byte[128];
+            int receivedBytes = 0;
+            int sendedBytes = 0;
+            string receivedString, sendString;
+
+            while (true)
+            {
+
+                receivedBytes = client.Receive(buff);
+                Console.WriteLine("Numero di byte ricevuti: " + receivedBytes);
+                receivedString = Encoding.ASCII.GetString(buff, 0, receivedBytes);
+                Console.WriteLine("Stringa ricevuta: " + receivedString);
+
+                if (receivedString.ToUpper() == "QUIT")
+                {
+                    break;
+                }
+
+                Array.Clear(buff, 0, buff.Length);
+                sendedBytes = 0;
+
+                // crea il messaggio
+                sendString = "Benvenuto client";
+
+                // lo converto in byte
+                buff = Encoding.ASCII.GetBytes(sendString);
+
+                // invio al client il messaggio
+                sendedBytes = client.Send(buff);
+
+                Array.Clear(buff, 0, buff.Length);
+                                                
+            }  
+            
+            // Termina il programma
+                                                
+                                                
+        }                                       
+    }                                           
+}                                               
+                                                
