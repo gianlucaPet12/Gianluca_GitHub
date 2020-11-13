b762832615c1d3f02ae548eaa70f37e87d140b18
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-11-13 12:06:25 +0100

    Add project files.

---
+++ Gianluca_GitHub/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Gianluca_GitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Lister : in ascolto quando si parla dei server
+           // EndPoint 
+        }
+    }
+}
