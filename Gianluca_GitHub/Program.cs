368672162a6aa68566b4cc3e93e76168d3ca3790
gianluca pet <gian.arma@live.it>
2020-11-19 17:24:42 +0100

    Programma finito

--- Gianluca_GitHub/Program.cs
+++ Gianluca_GitHub/Program.cs
@@ -9,79 +9,98 @@ namespace Gianluca_GitHub
     {
         static void Main(string[] args)
         {
-            // Lister : in ascolto quando si parla dei server
-            // EndPoint : identifica una coppia IP/Porta
+            // Lister: in ascolto quando si parla dei server
+            // EndPoint: identifica una coppia IP/Porta
 
-            // Creare il mio socketlistene
-            // 1) specifico che versione ip
-            // 2) il tipo di socket. Stream.
-            // 3) protocollo a livello di trasporto
+            // Creare il mio socketlistener
+            //1) specifico che versione IP
+            //2) tipo di socket. Stream.
+            //3) protocollo a livello di trasporto
             Socket listenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
-            // confing: IP deve ascoltare. Possiamo usare l'opzione Any: ascolta da tutte le interfaccie all'interno del mio pc
+            // config: IP dove ascoltare. Possiamo usare l'opzione Any: ascolta da tutte le interfaccie all'interno del mio pc.
             IPAddress ipaddr = IPAddress.Any;
 
             // config: devo configurare l'EndPoint
             IPEndPoint ipep = new IPEndPoint(ipaddr, 23000);
 
-
             // config: Bind -> collegamento
             // listenerSocket lo collego all'endpoint che ho appena configurato
             listenerSocket.Bind(ipep);
 
-            // Mettere in ascolto il server
+            // Mettere in ascolto il server.
             // parametro: il numero massimo di connessioni da mettere in coda.
             listenerSocket.Listen(5);
-            Console.WriteLine("server in ascolto");
-            Console.WriteLine("In attesa di connessione da parte del client");
-
-            //istruzioni bloccante
-            //restituisce una variabile di tipo socket.
+            Console.WriteLine("Server in ascolto...");
+            Console.WriteLine("in attesa di connessione da parte del client...");
+            // Istruzione bloccante
+            // restituisce una variabile di tipo socket
             Socket client = listenerSocket.Accept();
 
             Console.WriteLine("Client IP: " + client.RemoteEndPoint.ToString());
 
-            // mi attrezzo per riceve un messaggio dal client
+            // mi attrezzo per ricevere un messaggio dal client
             // siccome è di tipo stream io riceverò dei byte, o meglio un byte array
             // riceverò anche il numero di byte.
             byte[] buff = new byte[128];
             int receivedBytes = 0;
             int sendedBytes = 0;
-            string receivedString, sendString;
+            string receivedString, sendString = "";
 
             while (true)
             {
-
                 receivedBytes = client.Receive(buff);
                 Console.WriteLine("Numero di byte ricevuti: " + receivedBytes);
+
                 receivedString = Encoding.ASCII.GetString(buff, 0, receivedBytes);
                 Console.WriteLine("Stringa ricevuta: " + receivedString);
 
-                if (receivedString.ToUpper() == "QUIT")
+                //if (receivedString.ToUpper() == "QUIT")
+                //{
+                //    break;
+                //}
+
+                if (receivedString != "\r\n")
                 {
-                    break;
+                    if (receivedString.ToUpper() == "QUIT")
+                    {
+                        break;
+                    }
+
+                    switch (receivedString.ToUpper())
+                    {
+                        case "COME STAI?":
+                            sendString = "BENE";
+                            break;
+
+                        case "CHE FAI?":
+                            sendString = "NIENTE";
+                            break;
+
+                        case "CIAO":
+                            sendString = "CIAO";
+                            break;
+
+                        default:
+                            sendString = "Non ho capito!!";
+                            break;
+                    }
+
+                    Array.Clear(buff, 0, buff.Length);
+                    sendedBytes = 0;
+
+                    //lo converto in byte
+                    buff = Encoding.ASCII.GetBytes(sendString);
+
+                    //invio al client il messaggio
+                    sendedBytes = client.Send(buff);
+
+                    Array.Clear(buff, 0, buff.Length);
                 }
+            }
 
-                Array.Clear(buff, 0, buff.Length);
-                sendedBytes = 0;
-
-                // crea il messaggio
-                sendString = "Benvenuto client";
-
-                // lo converto in byte
-                buff = Encoding.ASCII.GetBytes(sendString);
-
-                // invio al client il messaggio
-                sendedBytes = client.Send(buff);
-
-                Array.Clear(buff, 0, buff.Length);
-                                                
-            }  
-            
             // Termina il programma
-                                                
-                                                
-        }                                       
+        }
     }                                           
 }                                               
                                                 
